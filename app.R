720a0d5096f78bacbeb0ab817873d9f581eb2737
Tianjian Xie <111186868+TJXie68@users.noreply.github.com>
2022-11-21 16:31:18 -0500

    Add files via upload

--- app.R
+++ app.R
@@ -4,18 +4,36 @@ library(DT)
 library(shinyWidgets)
 library(tidyverse)
 library(magrittr)
+library(kableExtra)
+library(sf)
+library(readxl)
+library(rgdal)
+library(maptools) 
+library(sp) 
+library(leaflet)
 
-data <- read.csv("D:/R dataset/615/Cannabis_Registry.csv")
+data <- read.csv("Cannabis_Registry.csv")
 data <- data.frame(data)
 data <- data %>% rename(Longitude = x)
 data <- data %>% rename(Latitude = y)
 data$Latitude <- as.numeric(data$Latitude)
 data$Longitude <- as.numeric(data$Longitude)
+data %<>% rename(Application_Status = "\u9518\u7dfcpplication_Status")
 data %<>% select(ObjectId, updated_timestamp, SAM_ID, Application_Status, Facility_Address, Main_Entity_Name,
                  Type_of_Marijuana_License, Latitude, Longitude)
 
+
+spdata <- st_read("Boston_Neighborhoods.shp")
+map <- ggplot(spdata) + 
+  geom_sf(size = 1, color = "blue") + 
+  ggtitle("Boston") + 
+  coord_sf()+
+  geom_sf_text(aes(label = Name, geometry = geometry), color = 'red', size=2)+
+  theme_bw()
+
 ui <- fluidPage(
   sidebarLayout(mainPanel(navbarPage("Cannabis Registry", id="main",
+                                     tabPanel("Map",plotOutput("map")),
                                      tabPanel("Data", DT::dataTableOutput("data")))),
                 sidebarPanel(top = 50, right = 10,
                              pickerInput("Application Status", label = "Select a status:",
@@ -36,7 +54,10 @@ server <- function(input, output, session) {
       filter(data, Application_Status == input$Application_Status)
     }
   })
-  
+
+  output$map <- renderPlot(ggplot()+
+                                geom_sf(data=spdata,colour='#0C090A',fill=NA)+
+                                theme_bw())
   
   output$data <-DT::renderDataTable(datatable(
     data,filter = 'top',
